5f8a4e1699d3a638de42d3590c6139894af7a096
MiguelCLG <MiguelCLG@hotmail.com>
2022-04-25 16:14:51 +0100

    Algoritmo profundidade primeiro done

--- ProcuraConstrutiva.cs
+++ ProcuraConstrutiva.cs
@@ -14,6 +14,7 @@ class ProcuraConstrutiva {
     public static int expansoes = 0;
     public static int geracoes = 0;
     public static List<int> results = new List<int>();
+    public List<ProcuraConstrutiva> visitados = new List<ProcuraConstrutiva>();
 
     //Em termos de definição, o BFS usa uma fila (queue) para correr o algoritmos, uma lista é tecnicamente a mesma coisa e mais simples de usar
     public List<ProcuraConstrutiva> queue = new List<ProcuraConstrutiva>();
@@ -46,7 +47,35 @@ class ProcuraConstrutiva {
     public int ProfundidadePrimeiro(Stack<ProcuraConstrutiva> stack, List<ProcuraConstrutiva> visitados)
     {
 
-        return -1;
+        while (stack.Count() > 0)
+        {
+            // Verifica se o node atual tem a solução
+            ProcuraConstrutiva currentNode = stack.Pop();
+            if(currentNode.SolucaoCompleta())
+            {
+                currentNode.Debug();
+                return results.Last();
+            }
+            else{
+                // Verifica se o node não foi marcado como visitado
+                // Se não, então percorre o algoritmo e adiciona os filhos ao stack
+                // se foi visitado, então vai descartar esse node e vai ao seguinte no stack
+                if(!visitados.Contains(currentNode))
+                {
+                    List<ProcuraConstrutiva> nodes = new List<ProcuraConstrutiva>();
+                    nodes = currentNode.Sucessores(nodes);
+                    visitados.Add(currentNode);
+                    foreach (ProcuraConstrutiva sucessor in nodes)
+                    {
+                        stack.Push(sucessor);
+                    }
+                    int result = ProfundidadePrimeiro(stack, visitados);
+                    if(result > 0)
+                        return results.Last();
+                }
+            }
+        }
+        return -1; // nao encontrou solução
     }
     public void Expand(List<ProcuraConstrutiva> sucessores){
         geracoes++;
@@ -64,25 +93,38 @@ class ProcuraConstrutiva {
     public virtual void SolucaoVazia() {}
 	public virtual bool SolucaoCompleta() { return false; }    
     virtual public void Debug(){}
+
+    void LimpaTudo(){
+        stack.Clear();
+        queue.Clear();
+        stack.Push(this);
+        visitados.Clear();
+        expansoes = 0;
+        geracoes = 0;
+    }
+
     public void Teste(){
-        SolucaoVazia();
+        Console.Clear();
         while(true){
-            Console.Clear();
+            SolucaoVazia();
             Console.WriteLine("---------------------------------------------------------------------------------------------");
             Console.WriteLine("------------------------------Algoritmos Inteligencia Artificial-----------------------------");
             Console.WriteLine("---------------------------------------------------------------------------------------------");
             Console.WriteLine("[1] - Largura Primeiro | [2] - Profundidade Primeiro | [3] - Custo Uniforme* [Procuras Cegas]");
             Console.WriteLine("[0] - Sair                                                                          [Sistema]");
+            Console.WriteLine("---------------------------------------  Estatísticas  --------------------------------------");
+            Console.WriteLine("Expansoes: {0}                Geracoes: {1}", expansoes, geracoes);
             Console.WriteLine("---------------------------------------------------------------------------------------------");
             Console.Write("Opcao: ");
             
             string op = Console.ReadLine();
+            LimpaTudo();
             switch(op) {
                 case "1": 
                     Console.WriteLine("Largura Primeiro: " + LarguraPrimeiro().ToString());
                     break;
                 case "2": 
-                    Console.WriteLine("Largura Primeiro: " + LarguraPrimeiro().ToString());
+                    Console.WriteLine("Profundidade Primeiro: " + ProfundidadePrimeiro(stack, visitados).ToString());
                     break;
                 case "3": 
                     break;
